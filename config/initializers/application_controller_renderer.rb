59e6597efc41f67da318bc5504e13efd4231dba3
kouki9115 <kouki.n9115@gmail.com>
2021-03-04 12:26:01 +0000

    Start-2

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '5372c60a0eea4e4798aa9a0a48ed787ca3612e083f234594173786ce6b35dbded8e93ccd1366516930454eaeb082f67757743ab8851a28ec73e5b87912d2b7d9'
