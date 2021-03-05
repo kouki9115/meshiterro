ca784e7b8b863b2acea2770d049fcb7eb84d08cd
kouki9115 <kouki.n9115@gmail.com>
2021-03-05 04:40:30 +0000

    Start-7

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-devise_for :users
 
-root to:'homes#top'
+ devise_for :users
 
-resources :post_images, only: [:new, :create, :index, :show, :destroy]
+ root to:'homes#top'
 
+ resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+     resource :favorites, only: [:create, :destroy]
+ resources :post_comments, only: [:create, :destroy]
+ end
+ resources :users, only: [:show, :edit, :update]
 end
